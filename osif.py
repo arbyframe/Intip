c25916ae2ba0aa43e93d51e49caebac7093fb9aa
habibi <43301051+arbyframe@users.noreply.github.com>
2019-06-04 12:30:45 +0800

    Update osif.py

--- osif.py
+++ osif.py
@@ -73,14 +73,14 @@ def show_program():
                     %sINFORMATION%s
  ------------------------------------------------------
 
-    Author     Debby Anggraini 'CiKu370'
+    Author     'ArbyFamz'
     Name       OSIF 'Open Source Information Facebook'
-    CodeName   D3b2y
+    CodeName   toolnox
     version    full version
     Date       16/05/2018 09:35:12
     Team       Blackhole Security
-    Email      xnver404@gmail.com
-    Telegram   @CiKu370
+    Email      arbyframe@gmail.com
+    instagram  arby__f
 
 * if you find any errors or problems , please contact
   author
